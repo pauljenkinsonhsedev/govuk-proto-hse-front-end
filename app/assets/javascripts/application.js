cfc761c91f3089eabed7d9d6965d5e9317c0d640
Paul Jenkinson <61879828+pauljenkinsonhsedev@users.noreply.github.com>
2026-05-11 11:08:45 +0000

    Fix autocomplete to work with data-module pattern across all pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,13 @@
 window.GOVUKPrototypeKit.documentReady(() => {
-  const select = document.querySelector('#country')
-
-  if (select && window.accessibleAutocomplete) {
-    window.accessibleAutocomplete.enhanceSelectElement({
-      selectElement: select,
-      showAllValues: true,
-      defaultValue: '',
-      displayMenu: 'overlay'
-    })
-  }
+  const selects = document.querySelectorAll('select[data-module="autocomplete"]')
+  
+  selects.forEach((select) => {
+    if (window.accessibleAutocomplete) {
+      window.accessibleAutocomplete.enhanceSelectElement({
+        selectElement: select,
+        showAllValues: true,
+        displayMenu: 'overlay'
+      })
+    }
+  })
 })
